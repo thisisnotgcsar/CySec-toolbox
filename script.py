0c8a2de237db0052f7b3c5e6ed43f8fa6cee89d7
superuser <hiimgiulio@gmail.com>
2023-11-09 15:03:29 +0100

    now script contains an improved verion of the funciton send

--- script.py
+++ script.py
@@ -3,9 +3,28 @@ import time
 import subprocess
 from functools import reduce
 
+def int2bytes(num: int, adjust=8) -> bytes:
+	return num.to_bytes((num.bit_length() + 7) // 8, byteorder='little', signed=True if num < 0 else False).ljust(adjust, b"\x00")
+
+def any2bytes(datum):
+	if isinstance(datum, str):
+		return datum.encode()
+	elif isinstance(datum, int):
+		return int2bytes(datum)
+	elif isinstance(datum, bytes):
+		return datum
+	else:
+		raise TypeError("any2bytes: datum provided is neither str, int or bytes object")
+
 def send(r, data):
-	r.send(data)
-	time.sleep(0.1)
+	result=b""
+	if isinstance(data, list):
+		for datum in data:
+			result += any2bytes(datum)
+		else:
+			result = any2bytes(data)
+	r.sendline(result)
+	time.sleep(0.05)
 
 def print_red(str):
 	print("\033[91m" + str + "\033[0m")
@@ -16,17 +35,12 @@ def genbin(binary_name):
 		return bytes.fromhex(subprocess.check_output(command, shell=True, universal_newlines=True))
 	except (subprocess.CalledProcessError, ValueError) as e:
 		print(f"An exception occurred: {e}")
-		exit(-1)
+	exit(-1)
 
 def rop_chain(chain):	
 	for i in range(len(chain)):
 		chain[i] = p64(chain[i])
 	return reduce(lambda x, y: x + y, chain)
-
-# transforms an integer to bytes with the minimum number of bytes possible
-def int2bytes(num: int) -> bytes:
-	return num.to_bytes((num.bit_length() + 7) // 8, byteorder='little', signed=True if num < 0 else False)).ljust(8, b"\x00")
-
 
 #I/O functions
 
@@ -40,20 +54,19 @@ b_main = 0xffff
 one_gadget=0x00
 if args["ONE_GADGET"]:
 	one_gadget = int(args["ONE_GADGET"], 16)
-
 if args["REMOTE"]:
 	r = remote()
 else:
 	r = process()
-	if args["GDB"]:
-		gdb.attach(r, f"""
-		# b *{b_main}
-		unset env
-		set disable-randomization off
-		set debuginfod enabled on
-		c
-		""")
-		input("wait")
+		if args["GDB"]:
+			gdb.attach(r, f"""
+				# b *{b_main}
+				unset env
+				set disable-randomization off
+				set debuginfod enabled on
+				c
+				""")
+			input("wait")
 
 #LIBC = ELF("./path")
 #LIBC.address = 0xbase
