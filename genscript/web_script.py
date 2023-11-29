092348f1302f11f4a9065b252e841d790cde28f0
superuser <hiimgiulio@gmail.com>
2023-11-29 12:12:53 +0100

    added templates for pwn and web and improved arg3 ./ prepend string

---
+++ genscript/web_script.py
@@ -0,0 +1,38 @@
+import requests
+import string
+import random
+import re
+
+
+url = "url$1"
+
+def find_flag(data):
+	search = re.search("flag{[a-zA-Z1-9_!]+}", data)
+	if search:
+		print(search.group())
+	  
+
+def gen_ran_string(size=15):
+	return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(size))
+
+def register(s, u, p):
+	data = {"username": u, "password_1": p, "password_2": p, "reg_user": ""}
+	return s.post(url+"/register.php", data=data).text
+
+def login(s, u, p):
+	data = {"username": u, "password": p, "log_user": ""}
+	return s.post(url+"/login.php", data=data).text
+
+def upload_file(s, fieldname, file):
+	files = {fieldname: file}
+	return s.post(url+"/upload_user.php", files=files).text
+
+u = gen_ran_string()
+p = gen_ran_string()
+s = requests.Session()
+
+register(s, u, p)
+login(s, u, p)
+find_flag(upload_file(s, 'user_bak', ('payload', open('payload', 'rb'))))
+
+
